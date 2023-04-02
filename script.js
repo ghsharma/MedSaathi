3a7eb5b7708b35712d426083ac1cdaaeabfce648
Govind Harsh Sharma <95496933+ghsharma@users.noreply.github.com>
2023-04-02 08:05:03 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings:["scanning prescription","pill identification"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    var typed = new Typed(".typing-2", {
+        strings:["Deaths from overdose."],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
